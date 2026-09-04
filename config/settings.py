f8abd528c73b2fc525a1c4aee49b081eaf0f37f2
Vyankateshwar Pund <pundvyankateshwar@gmail.com>
2026-09-05 01:15:40 +0530

    chore: add .pythonanywhere.com to ALLOWED_HOSTS for cloud deployment

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-h8=wpq^w3^n)t&w(ll^&r6rf8!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = [h.strip() for h in os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1,.pythonanywhere.com").split(",") if h.strip()]
 
 # Application definition
 
